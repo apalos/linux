358125a12a2f62f264174e6a06185be31a2d2e74
Ilias Apalodimas <ilias.apalodimas@linaro.org>
2018-04-24 09:51:07 +0300

    cpsw: added switchdev support files
    Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

---
+++ drivers/net/ethernet/ti/cpsw_switchdev.h
@@ -0,0 +1,18 @@
+/*
+ * Texas Instruments switchdev Driver
+ *
+ * Copyright (C) 2012 Texas Instruments
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <net/switchdev.h>
+
+void cpsw_port_switchdev_init(struct net_device *ndev);
